6c3885ebb3b364d66e638959d4d2e1d0a8421a3c
Ryan Lindner <ryan.m.lindner@gmail.com>
2023-07-03 16:56:11 -0600

    lab completed

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,7 @@
 def oxford_comma(items):
-    return None
+    if(len(items) > 1):
+        items[-1] = f'and {items[-1]}'
+    if(len(items) == 2):
+        output = ' '.join(items)
+    else: output = ', '.join(items)
+    return output
